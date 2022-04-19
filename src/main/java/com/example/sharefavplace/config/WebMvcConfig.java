85b7f30535a959b3004e420e9e1ef362b8e6a940
kuu18 <kdai18.kuu@gmail.com>
2022-04-19 21:05:57 +0900

    フロントエンドとのAPI通信機能の追加

---
+++ src/main/java/com/example/sharefavplace/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.sharefavplace.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+  
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    String origin = "http://" + System.getenv("FRONT_URL");
+    registry.addMapping("/**")
+            .allowedOrigins(origin)
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+  }
+
+}
--- src/main/java/com/example/sharefavplace/controller/HelloWorld.java
+++ src/main/java/com/example/sharefavplace/controller/HelloWorld.java
@@ -1,12 +1,18 @@
 package com.example.sharefavplace.controller;
 
+import com.example.sharefavplace.repository.Greeting;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HelloWorld {
-  @RequestMapping("/")
-  public String name() {
-    return "Hello Docker World!!";
+
+  @RequestMapping("/hello")
+  public Greeting name() {
+    String content = "Hello Docker World!!";
+        System.out.println(System.getenv("API_DOMAIN"));
+    return new Greeting(1, content);
   }
 }
